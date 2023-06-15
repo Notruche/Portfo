2f06223f850a2854fd1922f40c268409c9f8c49f
Notruche <43958350+Notruche@users.noreply.github.com>
2023-06-15 20:54:31 +0200

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div class="about row container-fluid m-0 py-5 px-0">
+        <div class="col-2"></div>
+        <div class="col-2">
+            <img src="/images/TITRES/profil.png" alt="">
+            <a href="/images/cvartistiquePecot2023.pdf" target="blank">
+                <img src="/images/TITRES/telecharger.png" alt="">
+            </a>
+        </div>
+        <div class="col-6 px-5">
+            <h1 class="peko">Amélie Pécot / Pekolio</h1>
+            <h5>Mail : amelie.pecot25@gmail.com</h5>
+            <p class="py-4">Née en 1995, j’ai commencé comme tout le monde en maltraitant des 
+                feutres pour mes gribouillages. <br> J’ai visiblement beaucoup apprécié l’expérience car après 1 an de mise à niveau en arts appliqués du lycée Auguste Renoir à Paris, je me suis embarquée pour Bruxelles où j’ai décroché en 2019 un master en bande dessinée-édition à l’école supérieure des arts de Saint Luc. J’ai profité de ces études pour expérimenter et jouer avec différentes formes d’expression et de narration dans ce medium et manger beaucoup de frites. <br> Depuis, je prête mon crayon à toutes sortes de projets créatifs tout partageant ma passion pour l’art lors d’ateliers avec les petits et les grands!Le reste du temps, je fais de grandes balades à Bruxelles pour trouver de nouvelles idées :)
+            </p>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    </template>
