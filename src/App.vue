3c4ee629aa878e336618e5df9363052bdc2165fc
Notruche <corentin.pelaprat@gmail.com>
2024-02-05 16:37:22 +0100

    ajouts 2024

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+  <div class="navbar row container-fluid" 
+  style="background-image: url('/images/TITRES/fond.png'); background-size: contain">
+        <div class="col-4 hasimage p-4">
+          <router-link to="/">
+            <img src="/images/TITRES/ameliepecot.png" alt="" style="padding-left: 5%;">
+          </router-link>
+        </div>
+        <div class="col-5"></div>
+        <div class="col-3 row hasimage py-4">
+            <div class="col-3 hasimage d-flex flex-column justify-content-center align-items-center">
+              <router-link to="/about">
+                <img src="/images/TITRES/contact.png" alt="" class="align-middle">
+              </router-link>
+            </div>
+            <div class="col-3 hasimage d-flex flex-column justify-content-center align-items-center">
+              <a href="https://www.instagram.com/pekolio/" target="blank">
+                <img src="/images/TITRES/insta.png" alt="" class="align-middle">
+              </a>
+            </div>
+            <div class="col-3 hasimage d-flex flex-column justify-content-center align-items-center">
+              <a href="https://www.twitch.tv/gribouillos" target="blank">
+                <img src="/images/TITRES/twitch.png" alt="" class="align-middle">
+              </a>
+            </div>
+            <div class="col-3 hasimage d-flex flex-column justify-content-center align-items-center">
+              <a href="https://ko-fi.com/gribouillos/" target="blank">
+                <img src="/images/TITRES/shop.png" alt="" class="align-middle">
+              </a>
+            </div>
+        </div>
+    </div>
+
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      i: 0
+    };
+  },
+  methods: {   
+  },
+  created: function() {
+  }
+};
+</script>
+
---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="about row container-fluid m-0 py-5 px-0">
+        <div class="col-2"></div>
+        <div class="col-2">
+            <img src="/images/TITRES/profil.png" alt="">
+            <a href="/images/cvartistiquePecot2023.pdf" target="blank">
+                <img src="/images/TITRES/telecharger.png" alt="">
+            </a>
+        </div>
+        <div class="col-6 px-5">
+            <h1 class="peko">Amélie Pécot / Pekolio</h1>
+            <h5>Mail : amelie.pecot25@gmail.com</h5>
+            <p class="py-4">Amélie Pécot est une illustratrice et autrice de bande dessinée basée à Bruxelles. Elle aime imaginer des univers colorés et fourmillant de vie tout droit sortis de son esprit farfelu. <br><br>Après des études en bande dessinée à St-Luc Bruxelles, elle décide de prêter son crayon à toutes sortes de projets créatifs. Ses collaborations ont le plus souvent lieu avec des associations Bruxelloises, mais elle publie aussi dans la presse jeunesse et réalise de nombreux fanzines qu'elle distribue sur les marchés créatifs. <br><br>En parallèle, Amélie Pécot aime partager sa passion pour l'art lors d’ateliers avec les petits et les grands, en travaillant notamment avec Centre belge de la bande dessinée. <br><br>En 2020, elle crée l'ASBL Gribouillos avec Corentin Pelaprat et Christopher Boyd dans l'optique de développer leurs activités artistiques de manière collective tout en s'inscrivant dans le paysage associatif Bruxellois. En gardant comme point de départ la bande dessinée, leur collectif crée, édite des livres et organise des évènements culturels
+            </p>
+        </div>
+        <div class="col-2"></div>
+    </div>
+    </template>
